136aed41276e5ad0dce332eb4602eb2d33aa7d70
Erfan <erfanyarahmadi96@gmail.com>
2026-03-10 16:55:29 +0100

    Initial Commit

---
+++ PocketUp/PocketUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketUpApp.swift
+//  PocketUp
+//
+//  Created by Erfan Yarahmadi on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PocketUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
